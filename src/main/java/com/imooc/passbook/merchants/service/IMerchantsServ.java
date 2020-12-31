727b425c009cebc545ed52e4f19f74d9a6eccfca
justinzwd <1462724765@qq.com>
2020-12-31 12:59:32 +0800

    MerchantsApplication

--- src/main/java/com/imooc/passbook/merchants/service/IMerchantsServ.java
+++ src/main/java/com/imooc/passbook/merchants/service/IMerchantsServ.java
@@ -12,7 +12,7 @@ public interface IMerchantsServ {
     //创建商户服务
     Response createMerchants(CreateMerchantsRequest request);
 
-    //根据id构造商户信息
+    //根据id构造商户信息 buildMerchantsInfoById
     Response buildMerchantsInfoById(Integer id);
 
     //投放优惠券
