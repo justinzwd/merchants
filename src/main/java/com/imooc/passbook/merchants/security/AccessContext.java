7dd563dcc859a71fd2383abf43fae2fbc9f6cc37
justinzwd <1462724765@qq.com>
2021-01-02 19:04:02 +0800

    AccessContext

--- src/main/java/com/imooc/passbook/merchants/security/AccessContext.java
+++ src/main/java/com/imooc/passbook/merchants/security/AccessContext.java
@@ -2,7 +2,7 @@ package com.imooc.passbook.merchants.security;
 
 /**
  * 用 ThreadLocal 去单独存储每一个线程携带的Token信息
- * SpingBoot支持多线程并发
+ * SpringBoot支持多线程并发
  */
 public class AccessContext {
     private static final ThreadLocal<String> token = new ThreadLocal<>();
